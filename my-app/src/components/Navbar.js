3e4a5e0052ee9d4802d0f7f774d33431ca9591cd
Laura Maithya <laura.maithya@strathmore.edu>
2025-06-26 21:16:22 +0300

    Had to push this manually

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -6,6 +6,12 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">Hands2Heart</Link>
+        <div className="navbar-menu">
+          <Link to="/donate" className="navbar-link">Home</Link>
+          <Link to="/AboutUs" className="navbar-link">About</Link>
+          <Link to="/contact" className="navbar-link">Contact</Link>
+          <Link to="/donate" className="navbar-link">Donate</Link>
+        </div>
         <div className="navbar-links">
           <Link to="/signup" className="navbar-link">Sign Up</Link>
           <Link to="/login" className="navbar-link">Login</Link>
