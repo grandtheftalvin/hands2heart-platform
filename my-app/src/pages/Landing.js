6d5aab1c169264ada57c4b4359f998a5efc27562
LMaithya <laura.maithya@strathmore.edu>
2025-07-03 13:43:28 +0300

    I am now adding the images to the homepage

--- my-app/src/pages/Landing.js
+++ my-app/src/pages/Landing.js
@@ -2,6 +2,10 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import './Landing.css'; // <-- Import the CSS
  import givingHand from '../pages/orange-hand.jpg';// Adjust the path as necessary
+ import specialkid9 from '../pages/specialkid9.jpg';
+ import specialkid8 from '../pages/specialkid8.jpeg';
+  import specialkid7 from '../pages/specialkid7.jpg';
+   import specialkid6 from '../pages/specialkid6.jpeg';
 
 function Landing() {
   return (
@@ -23,14 +27,25 @@ function Landing() {
      <div className="logo-section">
             <img src={givingHand} alt="Giving hand logo" className="giving-hand-logo" />
      </div>
-      <h3 className="About-Us">Bidding for A Cause</h3>
+      <div className="first-section">
+        <h3 className="About-Us">Bidding for A Cause</h3>
+        <img src={specialkid9} alt="Special needs student" className="special-kid-image" />
+        <img src={specialkid8} alt="Special needs student" className="special-kid-image" />
+        <img src={specialkid7} alt="Special needs student" className="special-kid-image" />
+        <img src={specialkid6} alt="Special needs student" className="special-kid-image" />
+      </div>
+      
+
       <div className="logo-section">
             <img src={givingHand} alt="Giving hand logo" className="giving-hand-logo" />
      </div>
+
       <h3 className="About-Us">How it works</h3>
+
       <div className="logo-section">
             <img src={givingHand} alt="Giving hand logo" className="giving-hand-logo" />
      </div>
+
       <h3 className="About-Us">The Change it Makes</h3>
       
         
